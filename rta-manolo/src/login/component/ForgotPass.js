2df3fdbd687431b8490bb818065c367437d61a84
Alduin Carlo Magallones <magallonesalduin@gmail.com>
2023-04-16 13:07:07 +0800

    Added Forgot Password Initial

---
+++ rta-manolo/src/login/component/ForgotPass.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from '@mui/material';
+import InputBox from './InputBox';
+import bg from './../../Images/bg.png'
+
+function ForgotPass({onClick}) {
+    return (
+        <div>
+            <h2>Forgot Password Page</h2>
+        </div>
+    );
+}
+
+export default ForgotPass;
