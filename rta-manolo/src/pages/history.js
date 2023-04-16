0f69afd048b09eaf5e28703f8835a4ab1b6ee312
Sagrado, Princess Genevieve <xhanhime@gmai.com>
2023-04-16 13:53:54 +0800

    Created Chart for Record Dashboard Component

--- rta-manolo/src/pages/history.js
+++ rta-manolo/src/pages/history.js
@@ -5,8 +5,11 @@ function History(props) {
     return (
         <div>
             <Navbar></Navbar>
-            <div>
-                <h1>RECORD PAGE</h1>
+
+            
+
+            <div style={{backgroundColor:"#D9F3FF", boxShadow:"1px 1px 34px 1px #75D4FF", display:"flex", position:"absolute", width: "94rem", height: 60, justifyContent: "center", marginTop: "27rem"}}>
+                <h3>TRACK RECORD</h3>
             </div>
             <div style={{backgroundColor:"white", width:"94rem", height:"30rem", display:"flex", position:"absolute", marginTop: "35rem", borderRadius: 20, padding: 30, flexDirection:"column"}}>
                 <div style={{marginBottom: 50}}><TotalViolation></TotalViolation></div>
