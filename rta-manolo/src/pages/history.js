95f6e892f7c2907f2c5977fdc5a08d73656cd842
Sagrado, Princess Genevieve <xhanhime@gmai.com>
2023-04-16 13:52:13 +0800

    Added Total Violation Component to Record

--- rta-manolo/src/pages/history.js
+++ rta-manolo/src/pages/history.js
@@ -8,6 +8,13 @@ function History(props) {
             <div>
                 <h1>RECORD PAGE</h1>
             </div>
+            <div style={{backgroundColor:"white", width:"94rem", height:"30rem", display:"flex", position:"absolute", marginTop: "35rem", borderRadius: 20, padding: 30, flexDirection:"column"}}>
+                <div style={{marginBottom: 50}}><TotalViolation></TotalViolation></div>
+                <div style={{marginBottom: 50}}><TotalViolation></TotalViolation></div>
+                <div style={{marginBottom: 50}}><TotalViolation></TotalViolation></div>
+                <div style={{marginBottom: 50}}><TotalViolation></TotalViolation></div>
+                <div style={{marginBottom: 50}}><TotalViolation></TotalViolation></div>
+            </div>
             
         </div>
     );
