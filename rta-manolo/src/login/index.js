e8d7761d0e833c41c5b335d7cd9a84123ca9c752
Sagrado, Princess Genevieve <xhanhime@gmai.com>
2023-04-16 12:19:33 +0800

    Adding temporary Interface for Login

---
+++ rta-manolo/src/login/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function index(props) {
+    return (
+        <div>
+            <h1>LOGIN PAGE</h1>
+            <button>LOGIN</button>
+        </div>
+    );
+}
+
+export default index;
