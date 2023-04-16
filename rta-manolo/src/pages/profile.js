049049b93fadff97d46bee31825fff699e90d427
Alduin Carlo Magallones <magallonesalduin@gmail.com>
2023-04-16 13:24:33 +0800

    Added the rest of the Elements in Profile

--- rta-manolo/src/pages/profile.js
+++ rta-manolo/src/pages/profile.js
@@ -26,7 +26,51 @@ const Profiles = () => {
           <h1>Jayde Mike Engracia</h1>
         </div>
       </div>
-       
+        {edit ? (
+        <>
+        <div style={{position:"absolute", display:"flex", marginLeft: "17rem", marginTop:"15rem"}}>
+          <div>
+            <div style={{marginRight: 10, marginBottom: 20}}><InputBox disabled label="Jayde Mike"></InputBox></div>
+            <div><InputBox  disabled type="email" label="Email"></InputBox></div>
+          </div>
+          <div>
+            <div style={{marginRight: 10, marginBottom: 20}}><InputBox disabled  label="Engracia"></InputBox></div>
+            <div><InputBox disabled type="password"  label="Password"></InputBox></div>
+          </div>
+        </div>
+        </>
+        ):null}
+        
+        {save ?(
+        <>
+        <div style={{position:"absolute", display:"flex", marginLeft: "17rem", marginTop:"15rem"}}>
+          <div>
+            <div style={{marginRight: 10, marginBottom: 20}}><InputBox  label="Jayde Mike"></InputBox></div>
+            <div><InputBox  type="email" label="Email"></InputBox></div>
+          </div>
+          <div>
+            <div style={{marginRight: 10, marginBottom: 20}}><InputBox   label="Engracia"></InputBox></div>
+            <div><InputBox type="password"  label="Password"></InputBox></div>
+          </div>
+        </div>
+        </>):null}
+        <div>
+          {edit ? (
+            <>
+              <div style={{marginTop:"12rem", marginLeft: "17rem"}}><Button onClick={() => setEdit(!edit) & setSave(!save)} variant='contained'>EDIT</Button></div>
+            </>
+          ):null}
+
+          {save ? (
+            <>
+              <div style={{marginTop:"12rem", marginLeft: "17rem"}}><Button  style={{backgroundColor:"#00B050"}} onClick={() => setEdit(!edit) & setSave(!save)} variant='contained'>SAVE</Button></div>
+            </>
+          ):null}
+          
+          
+        </div>
+        
+        
         <div style={{marginTop: "30rem", marginLeft: "27rem", position:"absolute", display:"flex"}}>
           <img src={People}></img>
         </div>
