f4f967fb083686bb4825845ee547d89e358852b8
Alduin Carlo Magallones <magallonesalduin@gmail.com>
2023-04-16 13:22:59 +0800

    Added Initial Design on Profile

--- rta-manolo/src/pages/profile.js
+++ rta-manolo/src/pages/profile.js
@@ -1,16 +1,40 @@
-import React, { useState } from 'react';
 import Navbar from '../Navbar';
+import React, { useState } from 'react';
+import './css/home.css'
+import Profile from './../Images/profile.png'
+import InputBox from './../login/component/InputBox'
+import { Button } from '@mui/material';
+import { Edit } from '@mui/icons-material';
+import People from './../Images/people.png'
 
-function Profiles(props) {
-    return (
+const Profiles = () => {
+
+  const [save, setSave] = useState(false)
+  const [edit, setEdit] = useState(true)
+
+
+  return (
+    <>
+    <Navbar></Navbar>
+    <div className='ContainerCss'>
+      <div className='InnerContainer'>
+      <div style={{position:"absolute", display:"flex", marginLeft: "43.5rem", flexDirection:"column"}}>
         <div>
-            <Navbar></Navbar>
-            <div>
-                <h1>PROFILE PAGE</h1>
-            </div>
-            
+          <img src={Profile}></img>
         </div>
-    );
-}
-
+        <div style={{marginLeft: "-5rem"}}>
+          <h1>Jayde Mike Engracia</h1>
+        </div>
+      </div>
+       
+        <div style={{marginTop: "30rem", marginLeft: "27rem", position:"absolute", display:"flex"}}>
+          <img src={People}></img>
+        </div>
+        
+      </div>
+    </div>
+    </>
+  );
+};
+  
 export default Profiles;
